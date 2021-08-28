6f5355da96619d3a49d09436d0fd727e5040c0ce
BIG-MON <mrmon93+github@gmail.com>
2021-08-28 13:24:15 +0900

    feat: add main

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import AmazonBlock from "../../src";
+import { AmazonBlock } from "../../src";
 
-const App = () => <AmazonBlock />;
+const App = () => (
+  <AmazonBlock asin={"4908406766"} id={"TESTID"}>
+    ウィメンズウェア100年史
+  </AmazonBlock>
+);
 render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import React from "react";
 import "./style.css";
 
-const AmazonBlock = () => <h1>Hello from My Component</h1>;
-export default AmazonBlock;
+/** Amazon商品ブロック */
+export function AmazonBlock({ asin, children, id = "" }) {
+  const amazonUrl = `https://www.amazon.co.jp/gp/product/`;
+  const itemUrl = amazonUrl + asin + (id == "" ? "" : "/?tag=" + id);
+  const imageUrl = `https://images-na.ssl-images-amazon.com/images/P/${asin}.09.MZZZZZZZ`;
+
+  return (
+    <div className="amazon">
+      <div className="itemBox">
+        <div className="itemImage">
+          <a
+            href={itemUrl}
+            rel="noreferrer noopener external nofollow"
+            target="_blank"
+          >
+            <img src={imageUrl} alt={children} />
+          </a>
+        </div>
+        <div className="itemName">
+          <a
+            href={itemUrl}
+            rel="noreferrer noopener external nofollow"
+            target="_blank"
+          >
+            {children}
+          </a>
+        </div>
+        <div className="shopBrand">
+          <a
+            href={itemUrl}
+            rel="noreferrer noopener external nofollow"
+            target="_blank"
+          >
+            Amazon
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
