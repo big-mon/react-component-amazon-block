b0b3c06786b0446859c29a6363b1a7b38f06ebdc
BIG-MON <mrmon93+github@gmail.com>
2021-08-28 11:48:36 +0900

    first commit

---
+++ examples/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import AmazonBlock from "../../src";
+
+const App = () => <AmazonBlock />;
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./style.css";
+
+const AmazonBlock = () => <h1>Hello from My Component</h1>;
+export default AmazonBlock;
